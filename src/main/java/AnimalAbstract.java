e098de83e35a88e240c513961e85c7dc323ae893
Diane Ujeneza <dianesis17@gmail.com>
2019-09-23 10:40:13 +0200

    animal abtsract and interface

--- src/main/java/AnimalAbstract.java
+++ src/main/java/AnimalAbstract.java
@@ -1,2 +1,75 @@
+import org.sql2o.Connection;
+
 public class AnimalAbstract {
+    protected String name;
+    protected String age;
+    protected String health;
+    protected String type;
+    protected int id;
+
+    public String getName() {
+        return name;
+    }
+
+    //Method to get Age of Animal
+    public String getAge() {
+        return age;
+    }
+
+    //Method to get health of animal
+    public String getHealth() {
+        return health;
+    }
+
+    //Method to get Type0
+    public String getType() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Object)) {
+            return false;
+        }
+        AnimalAbstract myAnimal = (AnimalAbstract) otherAnimal;
+        return this.getName().equals(myAnimal.getName()) &&
+                this.getType().equals(myAnimal.getType()) &&
+                this.getId() == myAnimal.getId() &&
+                this.getAge() == myAnimal.getAge() &&
+                this.getHealth().equals(myAnimal.getHealth());
+    }
+
+    //Method for saving
+    public void save() {
+        try (org.sql2o.Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animal (name, age, health, type) VALUES (:name, :age, :health, :type);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("age", this.age)
+                    .addParameter("health", this.health)
+                    .addParameter("type", this.type)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
+
+    //Method to Find Id
+    public static AnimalAbstract find(int id) {
+        String sql = "SELECT * FROM animal WHERE id = :id;";
+        try (Connection con = DB.sql2o.open()) {
+            AnimalAbstract myAnimal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(AnimalAbstract.class);
+            return myAnimal;
+
+        }
+
+
+    }
 }
---
+++ src/main/java/AnimalInterface.java
@@ -0,0 +1,5 @@
+public interface AnimalInterface {
+    public boolean equals(Object otherObject);
+    public void save();
+    public  void delete();
+}
