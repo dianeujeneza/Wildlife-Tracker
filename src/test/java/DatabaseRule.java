059d981595c81fb5c51b1cdd7380971b12476942
Diane Ujeneza <dianesis17@gmail.com>
2019-09-23 11:48:24 +0200

    DB rule test failed

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,2 +1,23 @@
-public class DatabaseRule {
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.assertEquals;
+
+public class DatabaseRule extends ExternalResource {
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "1234");
+    }
+
+    @Override
+    protected void after() {
+        try (Connection con = DB.sql2o.open()) {
+            String deleteAnimalQuery = "DELETE FROM animal *;";
+            String deleteSightingQuery = "DELETE FROM sighting *;";
+            con.createQuery(deleteAnimalQuery).executeUpdate();
+            con.createQuery(deleteSightingQuery).executeUpdate();
+        }
+    }
 }
